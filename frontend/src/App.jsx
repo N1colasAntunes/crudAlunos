e4d1ea0f1811706783303977b2f18f1a070e5998
Nicolas Antunes Messias <NickZzz2019@gmail.com>
2026-05-21 20:28:02 +0000

    update

---
+++ frontend/src/App.jsx
@@ -0,0 +1,64 @@
+import { useEffect } from "react";
+import axios from "axios"
+import { useState } from "react";
+
+function App(){
+  const [alunos, setAlunos] = useState([])
+  const [nome, setNome] = useState("")
+  const [email, setEmail] = useState("")
+  const [curso, setCurso] = useState("")
+  const [editando, setEditando] = useState(false)
+  const [idAtual, setIdAtual] = useState(null)
+
+  async function salvar(e){
+    e.preventDefault()
+    const aluno = [nome, email, curso]
+
+    if(editando){
+      await axios.put(`https://supreme-space-waffle-r4rwg7wxvp4x256w-3001.app.github.dev/alunos/${idAtual}`,aluno)
+      setEditando(false)
+      setIdAtual(null)
+    } 
+    else{
+      await axios.post("https://supreme-space-waffle-r4rwg7wxvp4x256w-3001.app.github.dev/alunos",aluno)
+      //limparFormulario()
+      //buscarAlunos()
+
+    }
+  }
+
+  return(
+    <div style={{padding:20}}>
+      <h1>CRUD de Alunos</h1>
+
+      <form onSubmit={{salvar}}>
+        <input
+        type="text"
+        placeholder="Nome"
+        value={nome}
+        onChange={(texto)=>setNome(texto.target.value)}
+        />
+        <br/>
+        <input
+          type="email"
+          placeholder="E-mail"
+          value={email}
+          onChange={(texto)=>setEmail(texto.target.value)}
+          />
+        <br/>
+        <input
+         type="text"
+          placeholder="Curso"
+          value={curso}
+          onChange={(texto)=>setCurso(texto.target.value)}
+        />
+        <br/>
+        <button type="submit">
+          {editando ? "Atualizar" : "Cadastrar"}
+        </button>
+      </form>
+    </div>
+  )
+}
+
+export default App
